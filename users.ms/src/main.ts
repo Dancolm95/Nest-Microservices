24b95ed1650c864ecfea015432d82eff9cc921a3
Daniel Colona <dancolm95@gmail.com>
2025-05-21 13:11:26 -0500

    initial commit - monoreppo microservices

---
+++ users.ms/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: { host: '127.0.0.1', port: 8877 },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
